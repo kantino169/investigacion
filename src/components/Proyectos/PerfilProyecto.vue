4c9effc90708d3b2a99a85e995ed98dab10eabf7
unknown <kbzoncapo777@gmail.com>
2017-11-22 15:22:21 -0300

    a

--- src/components/Proyectos/PerfilProyecto.vue
+++ src/components/Proyectos/PerfilProyecto.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="width-1of6">
-    <div class="list">
-      <div class="item">
-        <div class="item-content">Proyecto</div>
-      </div>
-    </div>
+    <ul id="menuProyecto">
+      <li v-for="proyecto in proyectos" :key="proyecto.id">{{proyecto.nombre}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+const proyectos = [
+  {nombre: 'A'}, {nombre: 'B'}
+]
+
+export default {
+  data: () => ({ proyectos })
+}
+</script>
