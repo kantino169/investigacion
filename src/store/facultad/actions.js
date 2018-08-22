ee378a8c72b6866d3313e3a85aeccb887d803520
dragonslayer587 <kbzoncapo777@gmail.com>
2018-08-22 16:02:59 -0300

    l

--- src/store/facultad/actions.js
+++ src/store/facultad/actions.js
@@ -11,6 +11,43 @@ export async function crearFacultad ({commit}, {nombre}) {
   commit('agregar', {[facultad.id]: facultad})
 }
 
-export async function crearCarrera ({commit}, {nombre}, {}) {
-  const {data: carrera} = await axios.post('grupo', {nombre}, {facultad})
+export async function crearCarrera ({commit}, {nombre, idFacultad}) {
+  const {data: carrera} = await axios.post('carrera', {nombre, idFacultad})
+  commit('agregarCarrera', carrera)
+}
+
+export async function crearGrupo ({commit}, {nombre, idCarrera}) {
+  const {data: grupo} = await axios.post('grupo', {nombre, idCarrera})
+  commit('agregarGrupo', grupo)
+}
+
+export async function crearSubgrupo (store, {nombre, idGrupo}) {
+  const {data: subgrupo} = await axios.post('subgrupo', {nombre, idGrupo})
+  store.commit('agregarSubgrupo', subgrupo)
+}
+
+export async function crearLinea ({commit}, {nombre, idSubgrupo}) {
+  const {data: linea} = await axios.post('linea', {nombre, idSubgrupo})
+  commit('agregarLinea', linea)
+}
+
+export async function modificarFacultad ({commit}, {nombre, id}) {
+  await axios.put(`facultad/${id}`, {nombre})
+  commit('actualizar', {nombre, id, altura: 0})
+}
+
+export async function modificarCarrera ({commit}, {nombre, id}) {
+  commit('actualizar', {nombre, id, altura: 1})
+}
+
+export async function modificarGrupo ({commit}, {nombre, id}) {
+  commit('actualizar', {nombre, id, altura: 2})
+}
+
+export async function modificarSubgrupo ({commit}, {nombre, id}) {
+  commit('actualizar', {nombre, id, altura: 3})
+}
+
+export async function modificarLinea ({commit}, {nombre, id}) {
+  commit('actualizar', {nombre, id, altura: 4})
 }
--- src/store/facultad/mutations.js
+++ src/store/facultad/mutations.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
+const path = ['carrera', 'grupo', 'subgrupo', 'linea']
+
 export function eliminarTodas (state) {
   for (const id in state) {
     Vue.delete(state, id)
@@ -10,4 +12,54 @@ export function agregar (state, facultades) {
   for (const id in facultades) {
     Vue.set(state, id, facultades[id])
   }
+}
+
+export function agregarCarrera (state, carrera) {
+  const carreras = state[carrera.id_facultad].carreras
+  Vue.set(carreras, carrera.id, carrera)
+}
+
+function getSubtree (tree, id, current, ...rest) {
+  if (current === undefined) {
+    return tree[id]
+  }
+  for (const idparent in tree) {
+    const value = getSubtree(tree[idparent][`${current}s`], id, ...rest)
+    if (value) return value
+  }
+}
+
+export function agregarGrupo (state, grupo) {
+  let carrera = getSubtree(state, grupo.id_carrera, 'carrera')
+  if (!carrera) return
+  Vue.set(carrera.grupos, grupo.id, grupo)
+}
+
+export function agregarSubgrupo (state, subgrupo) {
+  let grupo = getSubtree(state, subgrupo.id_grupo, 'carrera', 'grupo')
+  if (!grupo) return
+  Vue.set(grupo.subgrupos, subgrupo.id, subgrupo)
+}
+
+export function agregarLinea (state, linea) {
+  let subgrupo = getSubtree(state, linea.id_subgrupo, 'carrera', 'grupo', 'subgrupo')
+  if (!subgrupo) return
+  Vue.set(subgrupo.lineas, linea.id, linea)
+}
+
+export function actualizar (state, {id, nombre, altura}) {
+  if (altura < 0 || altura > 3) throw Error('Profundidad incorrecta')
+  const subtree = getSubtree(state, id, ...path.slice(0, altura))
+  subtree.nombre = nombre
+}
+
+export function eliminar (state, {id, altura}) {
+  let parent
+  if (altura === 0) {
+    parent = state
+  } else {
+    const subtree = getSubtree(state, id, ...path.slice(0, altura - 1))
+    parent = subtree[`${path[altura - 1]}s`]
+  }
+  Vue.delete(parent, id)
 }
