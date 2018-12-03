ec44a737a800731ccf650397cbc2919f7fa38723
Adrian Hilt <adrihilt@hotmail.com>
2018-12-03 15:06:01 -0300

    Cambio

--- src/pages/Evaluadores/Evaluadores.vue
+++ src/pages/Evaluadores/Evaluadores.vue
@@ -3,7 +3,7 @@
     <q-list striped highlight>
       <q-list-header>Evaluadores</q-list-header>
       <q-item v-for="evaluador in evaluadores" :key="evaluador.id">
-        <q-item-main :label="evaluador.nombre + evaluador.apellido">
+        <q-item-main :label="evaluador.nombre + ' ' + evaluador.apellido">
         </q-item-main>
       </q-item>
     </q-list>
