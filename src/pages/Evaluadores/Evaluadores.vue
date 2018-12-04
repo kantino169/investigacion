c359427f3404ab5944268b597d30ffdc89ba5f7d
Adrian Hilt <adrihilt@hotmail.com>
2018-12-03 23:19:40 -0300

    Evaluadores

--- src/pages/Evaluadores/Evaluadores.vue
+++ src/pages/Evaluadores/Evaluadores.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>
-    <q-list highlight>
+  <div class="q-mx-xl column" >
+    <q-list class="q-mt-sm" separator>
+      <q-btn class="q-ml-sm" label="Agregar" icon="create" @click="agregar()"></q-btn>
       <q-list-header>Evaluadores</q-list-header>
-      <q-item v-for="evaluador in evaluadores" :key="evaluador.id">
-        <q-item-main :label="evaluador.nombre + ' ' + evaluador.apellido">
-        </q-item-main>
-        <q-item-side :label="evaluador.email"></q-item-side>
-      </q-item>
+      <q-collapsible highlight v-for="evaluador in evaluadores" :key="evaluador.id" :label="evaluador.nombre + ' ' + evaluador.apellido"
+      :sublabel="evaluador.profesion ">
+        <q-item>
+          <q-item-main :label="'Nombre: ' + evaluador.nombre + ' ' + evaluador.apellido"></q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-main :label="'Email: ' + evaluador.email"></q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-main :label="'Telefono: ' + evaluador.telefono"></q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-main :label="'Profesion: ' + evaluador.profesion"></q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-main :label="'Especialidad: ' + evaluador.especialidad"></q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-main :label="'Dni: ' + evaluador.dni"></q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-main :label="'Lugar de Trabajo: ' + evaluador.lugarTrabajo"></q-item-main>
+        </q-item>
+        <q-btn-group class="q-ml-md">
+          <q-btn color="orange" title="Editar" icon="edit" @click="editar(evaluador)"/>
+          <q-btn color="red" title="Eliminar" icon="delete" @click="borrar(evaluador)"/>
+        </q-btn-group>
+      </q-collapsible>
     </q-list>
   </div>
 </template>
--- src/store/evaluador/getters.js
+++ src/store/evaluador/getters.js
@@ -1,4 +1,12 @@
-const armarLista = state => id => ({id, nombre: state[id].nombre, apellido: state[id].apellido, email: state[id].email})
+const armarLista = state => id => ({id,
+  nombre: state[id].nombre,
+  apellido: state[id].apellido,
+  email: state[id].email,
+  profesion: state[id].profesion,
+  especialidad: state[id].especialidad,
+  telefono: state[id].telefono,
+  dni: state[id].dni,
+  lugarTrabajo: state[id].lugar_de_trabajo})
 
 export const evaluadores = state =>
   Object.keys(state).map(armarLista(state))
