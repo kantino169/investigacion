a3fb9f7fd2f52195ea874f65d4b29bbc3c292136
dragonslayer587 <kbzoncapo777@gmail.com>
2018-12-03 23:07:36 -0300

    proyecto

--- src/pages/Proyectos/Proyectos.vue
+++ src/pages/Proyectos/Proyectos.vue
@@ -1,4 +1,4 @@
-d<template>
+<template>
   <div class="row justify-center">
     <q-stepper ref="stepper" color="primary" v-model="step" :alternative-labels="alt" vertical>
       <q-step default name="parte1" title="I. IDENTIFICACION DEL PROYECTO">
