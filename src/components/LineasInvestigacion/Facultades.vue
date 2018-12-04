e1a03d6cefa730f7b009e0deebc29153abdf4831
dragonslayer587 <kbzoncapo777@gmail.com>
2018-12-04 11:25:38 -0300

    mañana con Martin

--- src/components/LineasInvestigacion/Facultades.vue
+++ src/components/LineasInvestigacion/Facultades.vue
@@ -4,18 +4,54 @@
     @set="$emit('set', $event)"
     @edit="$emit('edit', $event)"
     @remove="$emit('remove', $event)"
-    @add="$emit('add')">
+    @add="agregar">
     No hay facultades actualmente
   </lista-con-botones>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import ListaConBotones from './ListaConBotones'
 export default {
   components: {ListaConBotones},
   computed: {
     ...mapGetters('facultad', ['facultades'])
+  },
+  methods: {
+    ...mapActions('facultad', ['crearFacultad', 'modificarFacultad', 'eliminar']),
+    async agregar () {
+      try {
+        const nombre = await this.$q.dialog({
+          title: 'Nueva facultad',
+          message: 'Nombre:',
+          prompt: {model: '', type: 'text'}
+        })
+        await this.crearFacultad({nombre})
+      } catch (error) {
+      }
+    },
+    async editar ({id, nombre: nombreAnterior}) {
+      try {
+        const nombre = await this.$q.dialog({
+          title: 'Modificar facultad',
+          message: 'Nombre:',
+          prompt: {model: nombreAnterior, type: 'text'}
+        })
+        await this.modificarFacultad({id, nombre})
+      } catch (error) {
+      }
+    },
+    async remover ({id, nombre}) {
+      try {
+        await this.$q.dialog({
+          message: `¿Desea eliminar la disciplina ${nombre}?`,
+          ok: 'Aceptar',
+          cancel: 'Cancelar'
+        })
+        this.eliminar({id, tipo: 'facultad'})
+      } catch (error) {
+      }
+    }
   }
 }
 </script>
