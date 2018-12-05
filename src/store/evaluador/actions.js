ac067ceed9aa00b44169be036de844ee8811b5a3
arandaschimpf <arandaschimpf@users.noreply.github.com>
2018-12-05 09:31:18 -0300

    Fix crear acciones

--- src/store/evaluador/actions.js
+++ src/store/evaluador/actions.js
@@ -1,22 +1,67 @@
 import axios from 'axios'
 
-export async function cargarTodos ({commit}) {
-  const {data: evaluadores} = await axios.get('evaluadores')
+export async function cargarTodos({ commit }) {
+  const { data: evaluadores } = await axios.get('evaluadores')
   commit('agregar', evaluadores)
 }
 
-export async function crear ({commit}, {nombre, apellido, email, telefono, profesion, especialidad, dni, lugarTrabajo}) {
-  const {data: evaluador} = await axios.post('evaluadores', {nombre, apellido, email, telefono, profesion, especialidad, dni, lugarTrabajo})
-  commit('agregar', {[evaluador.id]: evaluador})
+export async function crear(
+  { commit },
+  {
+    nombre,
+    apellido,
+    email,
+    telefono,
+    profesion,
+    especialidad,
+    dni,
+    lugarTrabajo
+  }
+) {
+  const { data: evaluador } = await axios.post('evaluadores', {
+    nombre,
+    apellido,
+    email,
+    telefono,
+    profesion,
+    especialidad,
+    dni,
+    lugarTrabajo
+  })
+  commit('agregar', [evaluador])
 }
 
-export async function modificar ({commit}, {nombre, id, apellido, email, telefono, profesion, especialidad, dni, lugarTrabajo}) {
-  const {data: evaluador} = await axios.put(`evaluadores/${id}`, {nombre, apellido, email, telefono, profesion, especialidad, dni, lugarTrabajo})
+export async function modificar(
+  { commit },
+  {
+    nombre,
+    id,
+    apellido,
+    email,
+    telefono,
+    profesion,
+    especialidad,
+    dni,
+    lugarTrabajo
+  }
+) {
+  const { data: evaluador } = await axios.put(`evaluadores/${id}`, {
+    nombre,
+    apellido,
+    email,
+    telefono,
+    profesion,
+    especialidad,
+    dni,
+    lugarTrabajo
+  })
   commit('actualizar', evaluador)
 }
 
-export async function eliminar ({commit}, {id}) {
-  const {data: {success}} = await axios.delete(`evaluadores/${id}`)
-  if (success) commit('eliminar', {id})
+export async function eliminar({ commit }, { id }) {
+  const {
+    data: { success }
+  } = await axios.delete(`evaluadores/${id}`)
+  if (success) commit('eliminar', { id })
   return success
 }
